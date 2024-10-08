9d225e58d6ccf98435f758177126dd77db774e1d
Joshua Komaba <joshuakomaba@gmail.com>
2024-10-08 09:55:10 -0600

    Se agrega el componente para validar que el usuario tiene modulos para el formato ded inicio

--- app/dashboard/coordinacion/init/page.tsx
+++ app/dashboard/coordinacion/init/page.tsx
@@ -1,10 +1,12 @@
 import { CreateInitFormatForm } from "@/components/ui-custom/forms/create-init-format-form";
 import { Title } from "@/components/ui-custom/title";
+import { UserValidation } from "@/components/ui-custom/user-validation";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
 export default function initPage() {
     return (
         <div>
+            <UserValidation />
             <div className="flex flex-col md:flex-row items-center justify-between gap-5 mb-10">
                 <Title>Formato de Inicio</Title>
                 <Breadcrumb>
